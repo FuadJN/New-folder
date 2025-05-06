299932a5fe13e22af0a3c22c51f33312262871d7
FuadM-ov <clash.9455@gmail.com>
2025-05-06 18:51:18 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+// var x = 3;
+// var y = 2;
+// var z = x + y;
+// console.log(z);
+// alert('Hello World')
+//  var x = 2;
+//  var y = 3;
+//  var z = x * y;
+//  console.log(z)
+var eded = Number(prompt("eded daxil edin:"));
+alert(eded);
---
+++ main.ts
@@ -0,0 +1,14 @@
+// var x = 3;
+// var y = 2;
+// var z = x + y;
+// console.log(z);
+
+// alert('Hello World')
+
+//  var x = 2;
+//  var y = 3;
+//  var z = x * y;
+//  console.log(z)
+
+ var eded=Number(prompt("eded daxil et:"))
+ alert(eded)
